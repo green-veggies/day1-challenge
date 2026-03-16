7ba108aa5022b3b638e9c81806ad7ed01848aa32
green-veggies <b322070@iiit-bh.ac.in>
2026-03-16 18:42:43 +0530

    changes from feature-update branch

--- teams.py
+++ teams.py
@@ -1,7 +1,7 @@
 def greet():
-    print('Hi, welcome to git and github session')
+    print('Hi, welcome to git and github session, updated in feature-update branch')
 
 def farewell():
-    print('Bye, now go home')
+    print('Bye, now go home, updated in feature-update branch')
 greet()
 farewell()
